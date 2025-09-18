76a81631b479d72813697f37a780f3cf8b8d73f1
sugirangendahayo <Sugirangendahayo835@gmail.com>
2025-09-18 09:34:55 +0200

    first initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // look for classes in all html files in this folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
